cfc72ab6d778caecb9a6a7629ef3b3d62829650e
Julian Adrien-Stark <119699025+julesstark22@users.noreply.github.com>
2023-02-22 15:07:13 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,61 +1,44 @@
 import csv
+from collections import Counter
 
-#Set up variables to store financial data
-total_months = 0
-total_profit_loss = 0
-prev_profit_loss = 0
-change_profit_loss = 0
-greatest_increase = ['', 0]
-greatest_decrease = ['', 999999999999]
+#Set the file path
+file_path = "election_data.csv"
 
-#Open the CSV file containing the financial data
-with open('budget_data.csv', 'r')as file:
-    reader = csv.reader(file)
+#Initialize variables
+total_votes = 0
+candidates = []
 
+#Read the csv file and iterate over rows
+with open(file_path, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
     #Skip the header row
-    header = next(reader)
+    next(csvreader)
+    for row in csvreader:
+    #Increment the vote count
+        total_votes += 1
+    #Add the candidate to the list of candidates
+    candidates.append(row[2])
+    
+#Count the number of votes for each candidate
+vote_count = Counter(candidates)
 
-    #Loop through each row in the CSV file
-    for row in reader:
+#Calculate the percentage of votes for each candidate
+percentages = {}
+for candidate, count in vote_count.items():
+    percentage = round((count / total_votes) * 100,3)
+    percentages[candidate] = percentage
+    
+#Find the winner based on popular vote
+winner = max(vote_count, key=vote_count.get)
 
-        #Increment the total number of months
-        total_months += 1
-
-#Add the current profit/loss to the total
-total_profit_loss += int(row[1])
-
-#Calculate the change in profit/loss from the previous month
-if total_months > 1:
-    change_profit_loss = int(row[1]) - prev_profit_loss
-
-#Update the previous profit/loss to the current one
-prev_profit_loss = int(row[1])
-
-#Check if this change is the greatest increase or decrease
-if change_profit_loss > greatest_increase[1]:
-    greatest_increase[0] = row[0]
-    greatest_increase[1] = change_profit_loss
-
-if change_profit_loss < greatest_decrease[1]:
-    greatest_decrease[0] = row[0]
-    greatest_decrease[1] = change_profit_loss
-
-#Calculate the average change in profit/loss
-average_change = round((total_profit_loss / total_months), 2)
-
-#Print out the financial analysis
-print("Financial Analysis")
-print("-------------------------")
-print(f"Total Months: {total_months}")
-print(f"Total: ${total_profit_loss}")
-print(f"Average Change: ${average_change}")
-print(f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})")
-print(f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})")
-
-#Save the ouput to a text file
-with open("main.py.txt", "w")as file:
-    file.write(output)
-
-#Print the output to the console
-print(output)
+#Print the analysis results
+print("Election Results")
+print("--------------------------")
+print(f"Total Votes: {total_votes}")
+print("--------------------------")
+for candidate, count in vote_count.items():
+    print(f"{candidate}: {percentages[candidate]}% ({count})")
+print("--------------------------")
+print(f"Winner: {winner}")
+print("--------------------------")
 
